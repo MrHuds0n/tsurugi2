dd0e3c709a7d769ec925c897229a1cd6fb36c0e9
mrhudson <protegegod@gmail.com>
2017-02-07 16:32:03 +0100

    Initial commit.

---
+++ script.js
@@ -0,0 +1,39 @@
+function $(q) {
+  return document.querySelector(q)
+}
+
+function $A(q) {
+  return document.querySelectorAll(q)
+}
+
+//*
+$(".left").addEventListener("click", (e) => {
+
+  for(element of e.target.parentNode.childNodes) {
+    if(e.target!=element)
+      element.classList.add("inactive")
+  }
+  e.target.classList.add("active")
+  e.target.childNodes[1].classList.remove("inactive")
+
+  e.target.childNodes[1].addEventListener("click", (f) => {
+    console.log(f.target);
+  })
+
+  /*
+  if(e.target===$("#corpid")) {
+    console.log(e.target);
+    let els = document.querySelectorAll(".left>.menu>*")
+    for(el of els) {
+      if(el!=$("#corpid")) {
+        el.classList.toggle("inactive")
+      }
+      else {
+        el.classList.toggle("active")
+      }
+    }
+    $(".card-create.corpid").classList.toggle("inactive")
+  }
+  //*/
+}, false)
+//*/
